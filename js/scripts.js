b0361eafa690ce03ca41e959e23fb4d7407403f0
jsy5888 <oliviasunjin0318@gmail.com>
2024-06-16 21:10:43 -0400

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
-//The user can add a new item to a list of items.
-let inputs = $('input');
-inputs.addClass('my-class');
 
-
-//The user can cross out an item from the list of items.
+$(document).ready(function(){
 
 
 
-//The user can delete an item from the list of items.
-input.remove(); // Delete the element
-$('body').append(input); // Like .appendChild
-$('body').empty(); // Remove all children of the element
+//add new item
+    function newItem(){
+        let li = $('<li></li>');
+        let inputValue = $('#input').val();
+        li.append(inputValue);
+
+    }
 
 
-//The user can change the order of items in the list of items.
+//cross out item
+    function cross(){
+        li.addClass("strike")
+    }
+        
+
+
+//Delete item
+    let deleteButton = $("<deleteButton></deleteButton>");    
+    deleteButton.append("X");
+    li.addClass("deleteButton");
+
+//change the order
+    $("#list").sortable();
+
+});

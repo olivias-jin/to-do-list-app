5889ee053ca1bda96b826e056202663a235cc2d1
jsy5888 <oliviasunjin0318@gmail.com>
2024-06-16 21:40:20 -0400

    uoate

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,41 @@
 
-$(document).ready(function(){
-
-
-
 //add new item
     function newItem(){
         let li = $('<li></li>');
         let inputValue = $('#input').val();
         li.append(inputValue);
 
-    }
+        if (inputValue === ''){
+            alert("You must write something");
+        } else {
+            $('#list').append(li);
+        }
+    
 
 
 //cross out item
     function cross(){
         li.addClass("strike")
     }
+
+    li.on("dblclick", function cross (){
+        li.addClass("strike");
+    });
         
 
 
 //Delete item
-    let deleteButton = $("<deleteButton></deleteButton>");    
-    deleteButton.append("X");
-    li.addClass("deleteButton");
+    let deleteButton = $('<deleteButton></deleteButton>');    
+
+    deleteButton.append(document.createTextNode('X'));
+    li.attend(deleteButton);
+
+    deleteButton.on("click",deleteListItem);
+    function deleteListItem(){
+        li.addClass("delete")
+    }
 
 //change the order
     $("#list").sortable();
 
-});
+}
